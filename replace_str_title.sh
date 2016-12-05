247d3813233987302bf63e03834642d81ee898a1
Michael Vershinin <michaelv@gettaxi.com>
2016-12-05 17:11:51 +0200

    minor fixes

--- replace_str_title.sh
+++ replace_str_title.sh
@@ -2,24 +2,24 @@
 #!/bin/bash
 # chmod +x ./scrum_role.sh
 
-src='$HOME/repos/chef'
-dst='$HOME'/Desktop/scrum_role/'
+src="${HOME}/repos/chef_copy"
+dst="${HOME}/Desktop/scrum_role/"
 scrum_name='scrum9'
 
 # create directory 
-if [ ! -d $dst ]; then
-    mkdir -p $dst 
-    echo "Your directory: $dst has created..."
+if [ ! -d ${dst} ]; then
+    mkdir -p ${dst} 
+    echo "Your directory: ${dst} has created..."
   else
-    echo "Your directory: $dst already exists..."
+    echo "Your directory: ${dst} already exists..."
 fi
 
 # copy role's files
-if [ ! "$(ls -A $dst)" ]; then
-    cp $src/roles/*/*${scrum_name}.rb $dst 
-    echo "Your files were copied to: $dst..."
+if [ ! "$(ls -A ${dst})" ]; then
+    cp ${src}/roles/*/*${scrum_name}.rb ${dst} 
+    echo "Your files were copied to: ${dst}..."
   else
-    echo "Your directory: $dst isn't empty..."
+    echo "Your directory: ${dst} isn't empty..."
 fi
 
 # set scrum number
@@ -31,29 +31,29 @@ if [ -z ${new_scrum} ]; then
 exit 1
 fi
 
-cd $dst
+cd ${dst}
 
 # rename scrum number
 for files in *.rb
 do
-	role_name=$(echo $files | cut -d '-' -f1)
+	role_name=$(echo ${files} | cut -d '-' -f1)
 
-	if [ $role_name == 'service' ]; then # for service-hub-scrum
-		role_name=$(echo $files | cut -d '-' -f1-2)
+	if [ ${role_name} == 'service' ]; then # for service-hub-scrum
+		role_name=$(echo ${files} | cut -d '-' -f1-2)
 		
-	elif [ $role_name == 'class' ]; then
-		role_name=$(echo $files | cut -d '-' -f1-2) # for class-[service]-scrum
+	elif [ ${role_name} == 'class' ]; then
+		role_name=$(echo ${files} | cut -d '-' -f1-2) # for class-[service]-scrum
 	fi	
 
-	sed "s/${scrum_name}/scrum${new_scrum}/g" $files > "$role_name-scrum$new_scrum.rb"
-	roles_path=`find $src/roles -name $files`
+	sed "s/${scrum_name}/scrum${new_scrum}/g" $files > "${role_name}-scrum${new_scrum}.rb"
+	roles_path=`find ${src}/roles -name ${files}`
  	echo "${role_name}-scrum${new_scrum}.rb"
 	#mv $files "$role_name-scrum$new_scrum.rb"
 	mv "${role_name}-scrum${new_scrum}.rb" $(dirname "${roles_path}")/
 done
 
 # remove destination folder after a previous tasks
-rm -rf $dst
+rm -rf ${dst}
 
 
 
